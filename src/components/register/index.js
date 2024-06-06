bb205c7d8217a35a199716ddc725425514b34367
MrSharma <MrSharma619@local.com>
2024-06-06 20:17:23 +0530

    registration page backend integration

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./style.css";
 import {
   MDBBtn,
@@ -7,8 +8,74 @@ import {
   MDBInput,
   MDBCheckbox,
 } from "mdb-react-ui-kit";
+import { toast } from "react-toastify";
+import api from "../../axios/apiConfig";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    userName: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    //console.log(e.target.checked);
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //console.log(formData);
+
+    if (formData.password === formData.repeatPassword) {
+      try {
+        delete formData.repeatPassword;
+
+        //console.log(formData);
+
+        const response = await api.post("/api/auth/register", formData);
+
+        //console.log(response);
+
+        if (response.status === 201) {
+          toast.success("Registration success. Please login now...");
+
+          setTimeout(() => {
+            setFormData({
+              fullName: "",
+              userName: "",
+              email: "",
+              password: "",
+              repeatPassword: "",
+            });
+
+            navigate("/login");
+          }, 3000);
+
+        }
+
+      } catch (error) {
+        console.log("Register-handleSubmit: ", error);
+
+        toast.error("Register failed! Please try again...");
+      }
+    } else {
+      toast.error("Passwords dont match. Please check again");
+    }
+  };
+
   return (
     <div className="parent-register-div">
       <MDBContainer
@@ -24,54 +91,72 @@ const Register = () => {
             <h2 className="text-uppercase text-center mb-5">
               Create an account
             </h2>
-            <MDBInput
-              wrapperClass="mb-4"
-              label="Your Full Name"
-              size="lg"
-              id="form1"
-              type="text"
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              label="Your Username"
-              size="lg"
-              id="form2"
-              type="text"
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              label="Your Email"
-              size="lg"
-              id="form3"
-              type="email"
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              label="Password"
-              size="lg"
-              id="form4"
-              type="password"
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              label="Repeat your password"
-              size="lg"
-              id="form5"
-              type="password"
-            />
-            <div className="d-flex flex-row justify-content-center mb-4">
-              <MDBCheckbox
-                name="flexCheck"
-                id="flexCheckDefault"
-                label="I agree all statements in"
+            <form onSubmit={handleSubmit}>
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Your Full Name"
+                size="lg"
+                id="form1"
+                type="text"
+                name="fullName"
+                value={formData.fullName}
+                onChange={handleChange}
               />
-              <a href="*" className="text-muted me-1 ms-1">
-                Terms of use
-              </a>
-            </div>
-            <MDBBtn className="mb-4 w-100" size="lg">
-              Register
-            </MDBBtn>
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Your Username"
+                size="lg"
+                id="form2"
+                type="text"
+                name="userName"
+                value={formData.userName}
+                onChange={handleChange}
+              />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Your Email"
+                size="lg"
+                id="form3"
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Password"
+                size="lg"
+                id="form4"
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Repeat your password"
+                size="lg"
+                id="form5"
+                type="password"
+                name="repeatPassword"
+                value={formData.repeatPassword}
+                onChange={handleChange}
+              />
+              <div className="d-flex flex-row justify-content-center mb-4">
+                <MDBCheckbox
+                  id="flexCheckDefault"
+                  label="I agree all statements in"
+                  name="terms"
+                  required
+                />
+                <a href="*" className="text-muted me-1 ms-1">
+                  Terms of use
+                </a>
+              </div>
+              <MDBBtn className="mb-4 w-100" size="lg" type="submit" block>
+                Register
+              </MDBBtn>
+            </form>
           </MDBCardBody>
         </MDBCard>
       </MDBContainer>
